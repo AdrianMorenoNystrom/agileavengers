510b045b0a8e8e8ce610be1b3048354e9113dd06
ericsallstrom <eric.sallstrom@gmail.com>
2024-02-28 08:54:04 +0100

    Display data in react

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
     const [data, setData] = useState(null);
+
     useEffect(() => {
         const callBackendAPI = async () => {
             try {
                 const response = await fetch('/api');
+
                 if (!response.ok) {
                     throw new Error('Failed to fetch data');
                 }
+
                 const body = await response.json();
                 setData(body.message);
             } catch (error) {
@@ -21,11 +23,27 @@ function App() {
     }, []);
     return (
         <div className="App">
-            <header className="App-header">
-                <img className="App-logo" src={logo} alt="logo" />
-                <h1 className="App-title">Welcome to React</h1>
-                <p style={{ color: 'white' }}>{data}</p>
-            </header>
+            <h1>People - Notion Data</h1>
+            <table className="table">
+                <div className="row-title">
+                    <div>Name</div>
+                    <div>Total Hours</div>
+                </div>
+                {data &&
+                    data.map((page) => {
+                        console.log(page); // Gör så att vi se objektet i konsolen.
+                        return (
+                            <div className="table-content" key={page.id}>
+                                <div className="names">
+                                    <p>{page?.properties?.Name?.title?.[0]?.text?.content}</p>
+                                </div>
+                                <div className="total-hours">
+                                    <p>{page?.properties?.['Total hours']?.rollup?.number}</p>
+                                </div>
+                            </div>
+                        );
+                    })}
+            </table>
         </div>
     );
 }
--- server/server.js
+++ server/server.js
@@ -1,8 +1,60 @@
+// const express = require('express');
+// const app = express();
+// const port = process.env.PORT || 3500;
+
+// app.get('/api', (req, res) => {
+//     res.send({ message: 'Hello from Express!' });
+// });
+// app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// const express = require('express');
+// const { NotionClient } = require('@notionhq/client');
+// const integrationToken = process.env.NOTION_INTEGRATION_TOKEN;
+// const pageId = process.env.NOTION_PAGE_ID;
+
+// const app = express();
+
+// const client = new NotionClient({
+//     auth: integrationToken,
+// });
+
+// app.get('/page/:pageId', async (req, res) => {
+//     try {
+//         const page = await client.getPage(req.params.pageId);
+//         res.json(page);
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ message: 'Error fetching page' });
+//     }
+// });
+
+// app.listen(3500);
+
 const express = require('express');
-const app = express();
+const dotenv = require('dotenv').config();
+const { Client } = require('@notionhq/client');
 const port = process.env.PORT || 3500;
 
-app.get('/api', (req, res) => {
-    res.send({ message: 'Hello from Express!' });
+const app = express();
+
+const notion = new Client({
+    auth: process.env.NOTION_TOKEN,
 });
-app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.get('/api', async (req, res) => {
+    const databaseId = process.env.NOTION_DATABASE_ID;
+
+    try {
+        const response = await notion.databases.query({ database_id: databaseId });
+        const relevantData = response.results;
+
+        res.json({ message: relevantData });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on ${port}`);
+});
