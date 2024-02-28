cb1cab06403e407e4e3d8888d821b64cbf13d307
ericsallstrom <eric.sallstrom@gmail.com>
2024-02-28 10:20:06 +0100

    fetch data from people-api

--- client/src/App.js
+++ client/src/App.js
@@ -21,6 +21,7 @@ function App() {
         };
         callBackendAPI();
     }, []);
+
     return (
         <div className="App">
             <h1>People - Notion Data</h1>
--- server/server.js
+++ server/server.js
@@ -1,35 +1,3 @@
-// const express = require('express');
-// const app = express();
-// const port = process.env.PORT || 3500;
-
-// app.get('/api', (req, res) => {
-//     res.send({ message: 'Hello from Express!' });
-// });
-// app.listen(port, () => console.log(`Listening on port ${port}`));
-
-// const express = require('express');
-// const { NotionClient } = require('@notionhq/client');
-// const integrationToken = process.env.NOTION_INTEGRATION_TOKEN;
-// const pageId = process.env.NOTION_PAGE_ID;
-
-// const app = express();
-
-// const client = new NotionClient({
-//     auth: integrationToken,
-// });
-
-// app.get('/page/:pageId', async (req, res) => {
-//     try {
-//         const page = await client.getPage(req.params.pageId);
-//         res.json(page);
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ message: 'Error fetching page' });
-//     }
-// });
-
-// app.listen(3500);
-
 const express = require('express');
 const dotenv = require('dotenv').config();
 const { Client } = require('@notionhq/client');
