8fa7e0a0b2213ce9149ec3766e3b35156309bf35
OlofEnar <olof.nordin@gmail.com>
2024-02-28 15:34:34 +0100

    Made a component of the fetching of data instead, with the "url" as argument to the server

---
+++ client/src/components/GetPeople.js
@@ -0,0 +1,33 @@
+import useFetchData from './UseFetchData';
+
+import React from 'react'
+
+export default function GetPeople() {
+    const data = useFetchData('/people');
+
+    return (
+        <>
+            <h1>People - Notion Data</h1>
+            <table className="table">
+                <div className="row-title">
+                    <div>Name</div>
+                    <div>Total Hours</div>
+                </div>
+                {data &&
+                    data.map((page) => {
+                        console.log(page); // Gör så att vi se objektet i konsolen.
+                        return (
+                            <div className="table-content" key={page.id}>
+                                <div className="names">
+                                    <p>{page?.properties?.Name?.title?.[0]?.text?.content}</p>
+                                </div>
+                                <div className="total-hours">
+                                    <p>{page?.properties?.['Total hours']?.rollup?.number}</p>
+                                </div>
+                            </div>
+                        );
+                    })}
+            </table>
+        </>
+    );
+}
