f2f6a3bb82a3517f4342bd7908c0c76d792a5e34
anon <etheoo@protonmail.com>
2024-03-01 09:15:53 +0100

    Refactor server.js and add routes as separate files

---
+++ server/routes/people/index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const { express, notion } = require("../../server");
+const router = express.Router();
+
+// Get all data from People Table
+router.get("/", async (req, res) => {
+  const databaseId = process.env.NOTION_DATABASE_ID;
+
+  try {
+    const response = await notion.databases.query({ database_id: databaseId });
+    const relevantData = response.results;
+
+    res.json({ message: relevantData });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+module.exports = router;
