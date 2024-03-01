4e26de9ff9e6118480b82b900dbafdb4ad6ad57b
anon <etheoo@protonmail.com>
2024-03-01 11:36:55 +0100

    Add meaningless spaces

--- server/notion.js
+++ server/notion.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+
 const { Client } = require("@notionhq/client");
 
 const notion = new Client({
--- server/routes/projects/addProject.js
+++ server/routes/projects/addProject.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+
 const { express } = require("../../express");
 const router = express.Router();
 const notion = require("../../notion");
