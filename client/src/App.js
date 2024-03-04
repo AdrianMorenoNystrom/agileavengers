cb8335f020b87e826da3307bf6aca1b9b6b348e6
OlofEnar <olof.nordin@gmail.com>
2024-03-04 16:31:33 +0100

    Added material

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import GetProjects from './components/GetProjects';
 import GetPeople from './components/GetPeople';
 import AddProject from './components/AddProject';
+
 function App() {
     return (
         <div className="App">
