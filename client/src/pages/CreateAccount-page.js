8b70f7cc0befbcc05bc7602987407bd31c2adfb8
AdrianMorenoNystrom <adrianmorenonystrom@gmail.com>
2024-03-06 17:20:12 +0100

    SubmitToNotion got own component to clean up code

--- client/src/pages/CreateAccount-page.js
+++ client/src/pages/CreateAccount-page.js
@@ -11,6 +11,7 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Alert from '@mui/material/Alert';
+import SubmitToNotion from '../components/SubmitUser';
 
 function Copyright(props) {
   return (
@@ -27,32 +28,7 @@ function Copyright(props) {
 
 const defaultTheme = createTheme();
 
-const SubmitToNotion = (fullName, email, password) => {
-  fetch('/people/add', {
-    method: 'post',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      fullName: fullName,
-      email: email,
-      password: password,
-    }),
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Failed to submit new user to Notion');
-      }
-      return response.json();
-    })
-    .then(() => {
-      console.log('Success!, new user created');
-    })
-    .catch((error) => {
-      console.log('Error!', error.message);
-    });
-};
+<SubmitToNotion/>
 
 export default function CreateAccount() {
   const [showSuccessAlert, setShowSuccessAlert] = React.useState(false);
