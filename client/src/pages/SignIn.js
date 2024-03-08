06336ad2d9370768243094cd0665a5f8e2619693
ericsallstrom <eric.sallstrom@gmail.com>
2024-03-08 11:56:22 +0100

    Users' can only sign in with email

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -54,7 +54,7 @@ export default function SignIn({ setIsAuthenticated }) {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          username: email,
+          username: email.trim(),
           password: password,
         }),
       });
