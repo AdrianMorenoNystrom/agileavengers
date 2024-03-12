8078cecdcd0e90322463f3e551ba202d28911903
OlofEnar <olof.nordin@gmail.com>
2024-03-12 12:23:17 +0100

    added component for testing DataGrid

---
+++ client/src/components/datatable/DataTable.js
@@ -0,0 +1,61 @@
+import "./datatable.scss"
+import React from "react";
+import { DataGrid } from '@mui/x-data-grid';
+
+const DataTable = () => {
+    const columns = [
+        { field: 'id', headerName: 'ID', width: 90 },
+        {
+          field: 'firstName',
+          headerName: 'First name',
+          width: 150,
+          editable: true,
+        },
+        {
+          field: 'lastName',
+          headerName: 'Last name',
+          width: 150,
+          editable: true,
+        },
+        {
+          field: 'age',
+          headerName: 'Age',
+          type: 'number',
+          width: 110,
+          editable: true,
+        },
+      ];
+      
+      const rows = [
+        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
+        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
+        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
+        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
+        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+        { id: 6, lastName: 'Melisandre', firstName: 'Bengt', age: 150 },
+        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+      ];
+    
+    
+    return (
+        <div className="dataTable">
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    initialState={{
+                    pagination: {
+                        paginationModel: {
+                        pageSize: 5,
+                        },
+                    },
+                    }}
+                    pageSizeOptions={[5]}
+                    disableRowSelectionOnClick
+                />
+        </div>
+    )
+}
+
+export default DataTable
