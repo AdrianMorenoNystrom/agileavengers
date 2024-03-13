cd201d6c0f9e889ac77021beec87ff0462063bd7
ericsallstrom <eric.sallstrom@gmail.com>
2024-03-13 11:42:23 +0100

    Fetching project to data table

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,19 +1,19 @@
 import React from "react";
 import "./home.scss";
-import ProjectOverview from "../../components/ProjectOverview";
 import ActiveProjects from '../../components/ActiveProjects';
+import GetPeopleNew from "../../components/getpeople/GetPeople";
 
 function Home() {
   return (
     <div className="home">
       <div className="box box1">
         <h2>Active projects</h2>
-        <ActiveProjects /> { }
+        <ActiveProjects />
       </div>
 
       <div className="box box2">
         <h2>Users</h2>
-        <ProjectOverview />
+        <GetPeopleNew />
       </div>
     </div>
   );
