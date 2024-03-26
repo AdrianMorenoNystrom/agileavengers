910a254170b51af5bb9ddccc48ea4da1f4c333fe
Patrik <144246738+PatpetEdu@users.noreply.github.com>
2024-03-26 17:50:13 +0100

    new loading for pages

--- client/src/components/GetAllProjectAvatars.js
+++ client/src/components/GetAllProjectAvatars.js
@@ -3,6 +3,8 @@ import Avatar from '@mui/material/Avatar';
 import useFetchData from './UseFetchData';
 import Tooltip from '@mui/material/Tooltip';
 import AvatarGroup from '@mui/material/AvatarGroup';
+import LoadingContext from './functions/LoadingContext'
+
 function stringToColor(string) {
     let hash = 0;
     for (let i = 0; i < string.length; i += 1) {
@@ -29,7 +31,7 @@ function generateAvatarInfo(data) {
 const GetAllProjectAvatars = ({ projectId, max, spacing }) => {
     const { data, isLoading, error } = useFetchData(`/api/projects/project/${projectId}`, true);
 
-    if (isLoading) return <div>Loading...</div>;
+    if (isLoading) return <LoadingContext/>;
     if (error) return <div>{error}</div>;
     if (!data) return <div>No data available</div>;
 
--- client/src/pages/project/Project.js
+++ client/src/pages/project/Project.js
@@ -22,7 +22,7 @@ import './project.scss';
 import TimeLine from '../../components/Timeline/TimeLine';
 import CategoryChart from '../../components/CategoryChart';
 import WeeklyReport from '../../components/GetWeeklyReport';
-import LoadingContext from '../../components/functions/LoadingContext';
+import PageLoadingContext from '../../components/functions/PageLoadingContext';
 
 function Project() {
     const { id } = useParams();
@@ -142,7 +142,7 @@ function Project() {
 
     const { data, isLoading, error } = useFetchData(`/api/projects/project/${id}`, true);
 
-    if (isLoading) return <LoadingContext/>;
+    if (isLoading) return <PageLoadingContext/>;
     if (error) return <div>{error}</div>;
     if (!data) return <div>No data available</div>;
 
